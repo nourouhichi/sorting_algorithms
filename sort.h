71f0a9ecca760c8c3bf794b5002663b0ebb11da2
nourouhichi <1417@holbertonschool.com>
2020-06-15 17:30:55 +0100

    new try

--- sort.h
+++ sort.h
@@ -1,5 +1,8 @@
 #ifndef SORT
 #define SORT
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -15,9 +18,12 @@ typedef struct listint_s
 } listint_t;
 
 /* mandatory tasks functions prototypes */
+void bubble_sort(int *array, size_t size);
 void insertion_sort_list(listint_t **list);
 void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void print_array(const int *array, size_t size);
 void print_list(const listint_t *list);
+void swap_int(int *array , int i);
+void inverse(listint_t *nexty);
 #endif
